c1f23565a38b1f6f1095c0e731216224ba6de669
Mykyta Vlasov <mykyta.vlasov@synerise.com>
2019-08-11 20:08:33 +0200

    feat: initial commit

---
+++ src/pages/about.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const About = () => (
+  <div>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint excepturi aspernatur asperiores sequi expedita
+    accusamus quos voluptatibus. Beatae quod accusamus praesentium similique saepe, tempore voluptate. Dignissimos,
+    assumenda neque. Vitae, dicta.
+  </div>
+);
